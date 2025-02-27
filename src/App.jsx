0505bbe50c8c104eb1269dd953c6fc40bc512ee9
Alex <atlexeide@gmail.com>
2025-02-27 15:09:46 +0100

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
+import Category from "./Components/Category";
+import { useState } from "react";
 
 function App() {
+  const categories = [
+    "Fiction",
+    "Mystery",
+    "Thriller",
+    "Romance",
+    "Fantasy",
+    "Morality",
+    "Society",
+    "Power",
+    "Justice",
+    "Adventure",
+    "Tragedy",
+    "War",
+    "Philosophy",
+  ];
+  const [cache, setCache] = useState({});
+  const [books, setBooks] = useState([]);
+
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route
+          exact
+          path="/"
+          element={<Home cache={cache} categories={categories} />}
+        />
+        <Route
+          path="/category/:id"
+          element={
+            <Category
+              cache={cache}
+              setCache={setCache}
+              books={books}
+              setBooks={setBooks}
+            />
+          }
+        />
       </Routes>
     </Router>
   );
