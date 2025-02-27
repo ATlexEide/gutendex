5417df38ea6e967b4e9469006cdbc4b491686b75
Alex <atlexeide@gmail.com>
2025-02-27 15:10:18 +0100

    Add header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,23 @@
-export default function Header() {
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+export default function Header({ categories }) {
+  const testState = { someKey: "someValue" };
+  window.history.pushState(testState, "page2.html");
+
   return (
     <header>
       <h1>Gutenberg oppgave</h1>
       <nav>
-        <ul></ul>
+        <ul id="categories">
+          {categories.map((cat, i) => (
+            <li key={i}>
+              <Link to={`/category/${cat}`} state={testState}>
+                {cat}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
