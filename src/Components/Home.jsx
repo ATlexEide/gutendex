fe036f6b408ee43bfa151ad62d9cdf58d1ae11da
Alex <atlexeide@gmail.com>
2025-02-27 15:10:32 +0100

    Update home return

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,10 @@
 import Header from "./Header";
 
-export default function Home() {
+export default function Home({ cache, categories }) {
+  console.log("Current cache: ", cache);
   return (
     <>
-      <Header />
+      <Header categories={categories} />
     </>
   );
 }
