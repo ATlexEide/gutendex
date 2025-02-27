afb876ad8e69c243b3784d58969ae87f450b3604
Alexander Eide <atlexeide@gmail.com>
2025-02-27 19:00:18 +0100

    Add details component

---
+++ src/Components/Details.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Details() {
+  const [book, setBook] = useState({});
+  const { id } = useParams();
+  console.log(id);
+  useEffect(() => {
+    fetch(`https://gutendex.com/books/${id}`)
+      .then((res) => res.json())
+      .then((res) => setBook(res));
+  }, [id]);
+  console.log(book);
+  return (
+    <>
+      <h1>Details</h1>
+      {book.title && <h2>{book.title}</h2>}
+      {book.formats && (
+        <img src={book.formats["image/jpeg"]} alt={`${book.title} cover`} />
+      )}
+      {/* {book.authors && (
+        <ul>
+          {book.authors.map((author, i) => (
+            <li key={i}>{author}</li>
+          ))}
+        </ul>
+      )} */}
+      {book.download_count && <p>Downloads: {book.download_count}</p>}
+      {book.category && <p>{book.category}</p>}
+      {book.languages && (
+        <ul>
+          <p>Languages:</p>
+          {book.languages.map((lang, i) => (
+            <li key={i}>{lang}</li>
+          ))}
+        </ul>
+      )}
+      {book.formats && (
+        <a target="_blank" href={book.formats["text/plain; charset=us-ascii"]}>
+          <button>Read</button>
+        </a>
+      )}
+      {book.formats && (
+        <a target="_blank" href={book.formats["application/epub+zip"]}>
+          <button>Download ePUB</button>
+        </a>
+      )}
+
+      <button>Add to favourites</button>
+    </>
+  );
+}
+export default Details;
