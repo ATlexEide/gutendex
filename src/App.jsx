58e6bee709900ddb4234d48514b993ed9b4715e6
Alex <atlexeide@gmail.com>
2025-02-27 12:56:18 +0100

    Add App return

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+export default App;
