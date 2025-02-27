ca6ca4de4a61d20fc7b8568f69283c3a0e891cf7
Alexander Eide <atlexeide@gmail.com>
2025-02-27 17:01:08 +0100

    Add conditional check

--- src/Components/Favourites.jsx
+++ src/Components/Favourites.jsx
@@ -1,11 +1,11 @@
 export default function Favourites({ favouriteBooks }) {
+  console.log(favouriteBooks);
   return (
     <>
       <h1>Favourites</h1>
       <ul>
-        {favouriteBooks.map((book, i) => (
-          <li key={i}>{book.title}</li>
-        ))}
+        {favouriteBooks &&
+          favouriteBooks.map((book, i) => <li key={i}>{book.title}</li>)}
       </ul>
     </>
   );
