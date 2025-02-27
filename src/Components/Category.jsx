ddc099da7d2a8e6ba6b371e67343c0a40cacba82
Alex <atlexeide@gmail.com>
2025-02-27 15:45:55 +0100

    Add pages

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -6,25 +6,30 @@ export default function Category({ cache, setCache }) {
   const { id } = useParams();
   console.log("Category: ", id);
   console.log("Current cache: ", cache);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(
+    cache[`${id}_page`] ? cache[`${id}_page`] : 1
+  );
 
-  const url = `https://gutendex.com/books?topic=${id}`;
+  const url = `https://gutendex.com/books/?page=${page}&topic=${id}`;
   // const url = `https://ponyapi.net/v1/character/all`;
 
+  console.log(cache[id], Boolean(cache[id]));
   useEffect(() => {
     if (!cache[id]) {
-      setIsLoading(true);
       fetch(url)
         .then((res) => res.json())
         .then((res) => {
           console.log(res.results);
-          setCache({ ...cache, [id]: res.results });
+          setCache({ ...cache, [`${id}_page`]: page, [id]: res.results });
           console.log("Updated cache: ", cache);
           setIsLoading(false);
         })
         .catch((e) => console.log(e));
+    } else {
+      setIsLoading(false);
     }
-  }, []);
+  }, [page]);
 
   return (
     <>
@@ -35,11 +40,35 @@ export default function Category({ cache, setCache }) {
         </>
       )}
       {cache[id] && (
-        <ul>
-          {cache[id].map((book, i) => (
-            <li key={i}>{book.title}</li>
-          ))}
-        </ul>
+        <>
+          <ul>
+            {cache[id].map((book, i) => (
+              <li key={i}>{book.title}</li>
+            ))}
+          </ul>
+          <div>
+            <button
+              onClick={() => {
+                if (page - 1 === 0) return;
+                setPage(page - 1);
+                setIsLoading(true);
+                setCache({ ...cache, [id]: undefined });
+              }}
+            >
+              Previous
+            </button>
+            <button>{page}</button>
+            <button
+              onClick={() => {
+                setPage(page + 1);
+                setIsLoading(true);
+                setCache({ ...cache, [id]: undefined });
+              }}
+            >
+              Next
+            </button>
+          </div>
+        </>
       )}
     </>
   );
