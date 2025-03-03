df66dbb0db7e89721ff457c1908b3216e9b729ba
Alex <atlexeide@gmail.com>
2025-03-03 10:10:00 +0100

    Add remove fav handler

--- src/js/handleFavourites.js
+++ src/js/handleFavourites.js
@@ -1,3 +1,8 @@
 export function addToFav(favouriteBooks, setFavouriteBooks, book) {
   setFavouriteBooks([...favouriteBooks, book]);
 }
+export function removeFromFav(favouriteBooks, setFavouriteBooks, book) {
+  const arr = favouriteBooks.splice();
+  console.log(arr);
+  setFavouriteBooks([...favouriteBooks, book]);
+}
