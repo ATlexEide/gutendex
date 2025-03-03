d7f22a100c4defbe6145d0a50f190dcb0d63dc7b
Alex <atlexeide@gmail.com>
2025-03-03 10:38:50 +0100

    Remove comments

--- src/js/handleFavourites.js
+++ src/js/handleFavourites.js
@@ -2,8 +2,5 @@ export function addToFav(favouriteBooks, setFavouriteBooks, book) {
   setFavouriteBooks([...favouriteBooks, book]);
 }
 export function removeFromFav(favouriteBooks, setFavouriteBooks, book) {
-  console.log(book);
-  console.log("favs", favouriteBooks);
   setFavouriteBooks(favouriteBooks.filter((_book) => _book.id !== book.id));
-  console.log("favs updated", favouriteBooks);
 }
