4d0a875bb6e269752bd513866a7f8993c4a0e85b
Alex <atlexeide@gmail.com>
2025-03-03 10:31:33 +0100

    Add logic for removal of favourite book

--- src/js/handleFavourites.js
+++ src/js/handleFavourites.js
@@ -2,7 +2,8 @@ export function addToFav(favouriteBooks, setFavouriteBooks, book) {
   setFavouriteBooks([...favouriteBooks, book]);
 }
 export function removeFromFav(favouriteBooks, setFavouriteBooks, book) {
-  const arr = favouriteBooks.splice();
-  console.log(arr);
-  setFavouriteBooks([...favouriteBooks, book]);
+  console.log("favs", favouriteBooks);
+  const arr = favouriteBooks.filter((_book) => (_book !== book ? book : null));
+  console.log("new arr", arr);
+  setFavouriteBooks([arr]);
 }
