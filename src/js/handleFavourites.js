267d4d5003423051b91ceca06ed1dddf068d058e
Alex <atlexeide@gmail.com>
2025-03-03 11:11:16 +0100

    Add localstorage logic

--- src/js/handleFavourites.js
+++ src/js/handleFavourites.js
@@ -1,6 +1,14 @@
 export function addToFav(favouriteBooks, setFavouriteBooks, book) {
   setFavouriteBooks([...favouriteBooks, book]);
+  localStorage.setItem(
+    "favourite_books",
+    JSON.stringify([...favouriteBooks, book])
+  );
 }
 export function removeFromFav(favouriteBooks, setFavouriteBooks, book) {
   setFavouriteBooks(favouriteBooks.filter((_book) => _book.id !== book.id));
+  localStorage.setItem(
+    "favourite_books",
+    JSON.stringify(favouriteBooks.filter((_book) => _book.id !== book.id))
+  );
 }
