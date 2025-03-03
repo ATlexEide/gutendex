04b24646a3d5181cfdb1eb5d2d6268f59f438a77
Alex <atlexeide@gmail.com>
2025-03-03 10:37:47 +0100

    Fix delete logic

--- src/js/handleFavourites.js
+++ src/js/handleFavourites.js
@@ -2,8 +2,8 @@ export function addToFav(favouriteBooks, setFavouriteBooks, book) {
   setFavouriteBooks([...favouriteBooks, book]);
 }
 export function removeFromFav(favouriteBooks, setFavouriteBooks, book) {
+  console.log(book);
   console.log("favs", favouriteBooks);
-  const arr = favouriteBooks.filter((_book) => (_book !== book ? book : null));
-  console.log("new arr", arr);
-  setFavouriteBooks([arr]);
+  setFavouriteBooks(favouriteBooks.filter((_book) => _book.id !== book.id));
+  console.log("favs updated", favouriteBooks);
 }
