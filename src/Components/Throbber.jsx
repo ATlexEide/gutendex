c2826c855592705fc4d1c8db34a8a55e5dfa4e55
Alex <atlexeide@gmail.com>
2025-03-04 13:02:57 +0100

    Add throbber component

---
+++ src/Components/Throbber.jsx
@@ -0,0 +1,10 @@
+import "./Throbber.css";
+function Throbber() {
+  return (
+    <div id="throbber">
+      <div className="loader"></div>
+      <p id="throbber-text">LOADING</p>
+    </div>
+  );
+}
+export default Throbber;
