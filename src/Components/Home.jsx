33e1babb483cb5ceefdb72e1270d3f0c7c8aeec3
Alex <atlexeide@gmail.com>
2025-03-04 11:36:46 +0100

    Add hero section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,3 +1,4 @@
+import "./Home.css";
 import Header from "./Header";
 
 export default function Home({ cache, categories, setSearchPage }) {
@@ -5,6 +6,10 @@ export default function Home({ cache, categories, setSearchPage }) {
   return (
     <>
       <Header setSearchPage={setSearchPage} categories={categories} />
+      <section id="hero">
+        <img src="./book.svg" alt="" />
+        <h2>SELECT A CATEGORY OR SEARCH FOR A BOOK</h2>
+      </section>
     </>
   );
 }
